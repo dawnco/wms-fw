b0f579e2c35887a84b88d5ea34157b7cd18b10bf
dawn <abke@qq.com>
2020-12-08 18:04:43 +0800

    new

--- app/conf/route.conf.php
+++ app/conf/route.conf.php
@@ -7,6 +7,5 @@
 return [
     // 'url 可用正则' => ['c' => '控制器', 'm' => '方法'];
     //"portal" => ["c" => \app\control\Portal::class, "m" => "index"],
-    "portal"          => ["c" => \app\control\PortalControl::class, "m" => "index"],
-    "repayment/(\d+)" => ['c' => \app\control\Repayment::class],
+    "portal" => ["c" => \app\control\PortalControl::class, "m" => "index"],
 ];
