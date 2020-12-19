7eff29ca85c021fb084bdaae5fc4a444ff6cd6ed
dawnco <abke@qq.com>
2020-12-19 17:11:54 +0800

    fx

--- wms/fw/Exception.php
+++ wms/fw/Exception.php
@@ -10,7 +10,7 @@ namespace wms\fw;
 class Exception extends \Exception
 {
 
-    public function __construct($message, $code = -1, $previous = null)
+    public function __construct($message, $code = 500, $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
