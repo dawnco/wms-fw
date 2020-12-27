69d2705704af9722de6f43eebd79b0e619da2d06
dawnco <abke@qq.com>
2020-12-27 11:20:16 +0800

    fx

---
+++ demo/App/www/build.js
@@ -0,0 +1,38 @@
+/**
+ * @author Dawnc
+ * @date   2020-12-27
+ */
+
+// 读取文件生成路由
+var path = require('path')
+var fs = require('fs')
+var jsFiles = []
+var routeFiles = []
+var pathName = './static/page/'
+fs.readdir(pathName, function (err, files) {
+
+  for (var i = 0; i < files.length; i++) {
+    jsFiles.push('<script src="/static/page/' + files[i] + '"></script>')
+    let name = files[i].replace('.js', '')
+    if (name != 'NotFound') {
+
+      routeFiles.push('{ path: \'/' + (name == 'Home' ? '' : name) + '\', component: ' + name + ' }')
+    }
+  }
+  routeFiles.push('{ path: \'*\', component: NotFound }')
+
+  let html = fs.readFileSync('index.template.html', 'utf8')
+
+  let str = html.replace('<!--page-->', '<!--page-->\n' + jsFiles.join('\n') + '\n<!--page-->')
+
+  fs.writeFileSync('index.html', str)
+
+  let js = fs.readFileSync('./static/router.js', 'utf8')
+
+  let str2 = 'const routes = [\n  ' +
+             routeFiles.join(',\n  ') +
+             '\n' +
+             ']'
+
+  fs.writeFileSync('./static/router.js', str2)
+})
--- demo/App/www/static/app.js
+++ demo/App/www/static/app.js
@@ -1,4 +1,12 @@
-﻿const app = new Vue({
+﻿
+
+// 3. 创建 router 实例，然后传 `routes` 配置
+// 你还可以传别的配置参数, 不过先这么简单着吧。
+const router = new VueRouter({
+  routes // (缩写) 相当于 routes: routes
+})
+
+const app = new Vue({
   router,
   store
 }).$mount('#app')
