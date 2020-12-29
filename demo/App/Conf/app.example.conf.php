f371cac8bc1963e2ae8f95748a8e7fb63e96f933
dawn <abke@qq.com>
2020-12-29 20:48:52 +0800

    fx

--- demo/App/Conf/app.example.conf.php
+++ demo/App/Conf/app.example.conf.php
@@ -4,12 +4,12 @@
  * @date   2020-12-08
  */
 return [
-    "env" => "dev",   // 环境 dev 开发  production 生产
-    "log" => [
+    "env"   => "dev",   // 环境 dev 开发  production 生产
+    "log"   => [
         'level' => "error", // 日志登录  debug info  warning error
         'dir'   => "", // 日志目录
     ],
-    "db"  => [
+    "db"    => [
         "default" => [
             "hostname" => "127.0.0.1",
             "username" => "root",
@@ -19,5 +19,12 @@ return [
             "charset"  => "utf8mb4",
         ],
     ],
+    'redis' => [
+        'default' => [
+            "hostname" => "127.0.0.1",
+            "port"     => 6379,
+            "password" => ""
+        ]
+    ]
 
 ];
