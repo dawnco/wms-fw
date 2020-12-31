4e0c4d9f076d57bdbe4d93f53b4c9f72bc470c48
dawn <abke@qq.com>
2020-12-31 20:37:57 +0800

    fx

--- demo/www/build.js
+++ demo/www/build.js
@@ -3,15 +3,33 @@
  * @date   2020-12-27
  */
 
+/**
+ * 正则
+ * https://developer.mozilla.org/zh-cn/docs/web/javascript/guide/regular_expressions
+ */
+
 // 读取文件生成路由
 var path = require('path')
 var fs = require('fs')
-var jsFiles = []
-var routeFiles = []
-var pathName = './static/page/'
-fs.readdir(pathName, function (err, files) {
 
-  for (var i = 0; i < files.length; i++) {
+var dir = __dirname.replace(/\\/g, '/')
+
+function msg (msg) {
+  console.log(msg)
+}
+
+// 生成路由和index.html
+function tplAndRoute (dir) {
+
+  let jsFiles = []
+  let routeFiles = []
+
+  let file = ''
+  let pathName = dir + '/static/page'
+
+  let files = fs.readdirSync(pathName)
+
+  for (let i = 0; i < files.length; i++) {
     jsFiles.push('<script src="/static/page/' + files[i] + '"></script>')
     let name = files[i].replace('.js', '')
     if (name != 'NotFound') {
@@ -21,17 +39,47 @@ fs.readdir(pathName, function (err, files) {
   }
   routeFiles.push('{ path: \'*\', component: NotFound }')
 
-  let html = fs.readFileSync('index.template.html', 'utf8')
+  let html = fs.readFileSync(dir + '/index.template.html', 'utf8')
 
   let str = html.replace('<!--page-->', '<!--page-->\n' + jsFiles.join('\n') + '\n<!--page-->')
 
-  fs.writeFileSync('index.html', str)
-
-
+  file = dir + '/index.html'
+  fs.writeFileSync(file, str)
+  msg('生成文件 ' + file)
   let str2 = 'const routes = [\n  ' +
              routeFiles.join(',\n  ') +
              '\n' +
              ']'
+  file = dir + '/static/router.js'
+  fs.writeFileSync(file, str2)
+  msg('生成路由 ' + file)
+}
 
-  fs.writeFileSync('./static/router.js', str2)
-})
+// vue 转js
+function vue2js (dir) {
+  let dirname = dir + '/static/vue'
+  let files = fs.readdirSync(dirname)
+
+
+  for (let i = 0; i < files.length; i++) {
+    let file = dirname + '/' + files[i]
+    let content = fs.readFileSync(file, 'utf8')
+
+    let match1 = /<template>(.+)<\/template>/gs.exec(content)
+    let tpl = match1[1]
+    console.log(tpl)
+
+    let match2 = /<script>(.+)<\/script>/gs.exec(content)
+    let js = match2[1]
+    let o = eval(js)
+    console.log(o)
+
+
+
+
+  }
+
+}
+
+// tplAndRoute(dir)
+vue2js(dir)
