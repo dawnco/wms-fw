833be57c10fed09d5874e188ea3c4a2c44d3d272
dawnco <abke@qq.com>
2021-01-16 10:15:31 +0800

    fx

--- demo/README.md
+++
@@ -1,9 +0,0 @@
-# 非编译 vue 应用
-
-先生成路由文件
-```
-node www/build.js
-```
-
-页面文件在 www/static/vue/page
-组件文件在 www/static/vue/page
--- demo/www/static/page/NotFound.js
+++
@@ -1,9 +0,0 @@
-const NotFoundPage = {
-  template: `<div style="width: 300px;margin: 100px auto 0 auto;border: 1px solid #CCCCCC;border-radius: 10px;padding: 20px;text-align: center">
-    <p>
-      404 页面不存在
-    </p>
-    <p>
-      <router-link to="/">首页</router-link>
-    </p>
-  </div>`,}
